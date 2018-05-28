4a834839a33566d5ce479d49081b7488fceee8f3
David Bucka <bucka@pier2port.de>
2018-05-28 23:03:15 +0200

    Reload if no_cache already exists

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-(function(){
+(function() {
     var getParams = window.location.search;
     var substring = "no_cache=1";
-    if ( getParams.indexOf(substring) === -1 ) {
-        if ( getParams.length > 0 ) {
+    if (getParams.indexOf(substring) === -1) {
+        if (getParams.length > 0) {
             var addParams = "&no_cache=1";
         } else {
-            addParams = "?no_cache=1"
+            addParams = "?no_cache=1";
         }
         window.location.href = window.location.href + addParams;
+    } else if (getParams.indexOf(substring) === 1) {
+        window.location = window.location;
     }
-})()
+})();
